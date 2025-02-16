483d64afddd1af45b59bef6eecd1e5e6b5d92659
AlejandroTR945 <atr945@educa.madrid.org>
2025-02-16 09:19:27 +0100

    ejercicio 7.2 finalizado

---
+++ Ejercicio8/start/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace WeatherClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRefresh_Clicked(object sender, EventArgs e)
+        {
+            btnRefresh.IsEnabled = false;
+            actIsBusy.IsRunning = true;
+
+            // Asigna el resultado al BindingContext en lugar de actualizar los controles directamente
+            BindingContext = await Services.WeatherServer.GetWeather(txtPostalCode.Text);
+
+            btnRefresh.IsEnabled = true;
+            actIsBusy.IsRunning = false;
+        }
+    }
+}
