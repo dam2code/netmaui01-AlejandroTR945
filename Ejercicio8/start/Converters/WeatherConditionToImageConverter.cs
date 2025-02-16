b1bfa26e6886c582a5667f3c027816ba4ef53e05
AlejandroTR945 <atr945@educa.madrid.org>
2025-02-16 09:26:44 +0100

    ejercicio 8.2 final

--- Ejercicio8/start/Converters/WeatherConditionToImageConverter.cs
+++ Ejercicio8/start/Converters/WeatherConditionToImageConverter.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
+using WeatherClient.Models;
 
-namespace WeatherClient.Converters
+namespace WeatherClient.Converters;
+
+internal class WeatherConditionToImageConverter : IValueConverter
 {
-    internal class WeatherConditionToImageConverter
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
+        WeatherType weatherCondition = (WeatherType)value!;
+
+        return weatherCondition switch
+        {
+            Models.WeatherType.Sunny => ImageSource.FromFile("sunny.png"),
+            Models.WeatherType.Cloudy => ImageSource.FromFile("cloud.png"),
+            _ => ImageSource.FromFile("question.png")
+        };
     }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
 }
